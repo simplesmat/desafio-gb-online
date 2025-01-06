712efdeb06b0229c1ce5911311410c5246fbdae2
Mat <mat.paulo24@gmail.com>
2025-01-06 11:56:27 -0300

    creating recipe components

---
+++ src/components/Recipe.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import {useState, useEffect} from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default function RecipeList() {
+    const [recipes, setRecipes] = useState([])
+
+}
